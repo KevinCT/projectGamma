ceb86ecca6e9fa5126787d5a6cb9c0951e91f848
KevinCT <kevinchentrieu@gmail.com>
2016-05-09 15:01:31 +0200

    Local authentication working

--- AndroidProject/Gasqueue/app/src/main/java/pekl/gasqueue/com/gasqueue/Activitiy/WelcomeActivity.java
+++ AndroidProject/Gasqueue/app/src/main/java/pekl/gasqueue/com/gasqueue/Activitiy/WelcomeActivity.java
@@ -2,8 +2,10 @@
 package pekl.gasqueue.com.gasqueue.Activitiy;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,19 +13,27 @@ import android.widget.EditText;
 import pekl.gasqueue.com.gasqueue.R;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private EditText passwordText;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        EditText codeInput= (EditText) findViewById(R.id.inputCodeField);
-
-        Button logInBtn = (Button) findViewById(R.id.logInBtn);
-        logInBtn.setOnClickListener(new View.OnClickListener() {
+        passwordText = (EditText) findViewById(R.id.inputCodeField);
+        loginBtn = (Button) findViewById(R.id.logInBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent temp = new Intent(WelcomeActivity.this,CategoryActivity.class);
-                startActivity(temp);
+                //local atm, change later
+                if(passwordText.getText().toString().equals("0000")) {
+                    Intent temp = new Intent(WelcomeActivity.this,CreateBarACtivity.class);
+                    startActivity(temp);
+                }
+                else if (passwordText.getText().toString().equals("1234")){
+                    Intent temp = new Intent(WelcomeActivity.this, CategoryActivity.class);
+                    startActivity(temp);
+                }
             }
         });
     }
