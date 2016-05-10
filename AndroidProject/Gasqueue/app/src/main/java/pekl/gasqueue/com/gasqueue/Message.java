8a66af30a304f1034c39652368c0f7733b129a58
KevinCT <kevinchentrieu@gmail.com>
2016-05-10 16:12:41 +0200

    Chat function working

---
+++ AndroidProject/Gasqueue/app/src/main/java/pekl/gasqueue/com/gasqueue/Message.java
@@ -0,0 +1,36 @@
+package pekl.gasqueue.com.gasqueue;
+
+
+
+/**
+ * Created by kevin on 09/05/2016.
+ */
+public class Message {
+    private String message;
+    private String name;
+    private String timeStamp;
+
+    //need for firebase:?
+    public Message() {
+
+    }
+
+    public Message(String message, String name, String timeStamp) {
+        this.message = message;
+        this.name = name;
+        this.timeStamp=timeStamp;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+}
