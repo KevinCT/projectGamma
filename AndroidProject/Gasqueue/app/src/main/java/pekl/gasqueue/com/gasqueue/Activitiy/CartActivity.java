bb9c77d17e5e27614b4c0d5d25f12ecc1664e972
longnktx <yh_long.kotex@yahoo.com.vn>
2016-05-18 12:41:35 +0200

    First version of CartActivity with Adapter

--- AndroidProject/Gasqueue/app/src/main/java/pekl/gasqueue/com/gasqueue/Activitiy/CartActivity.java
+++ AndroidProject/Gasqueue/app/src/main/java/pekl/gasqueue/com/gasqueue/Activitiy/CartActivity.java
@@ -2,14 +2,25 @@ package pekl.gasqueue.com.gasqueue.Activitiy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import pekl.gasqueue.com.gasqueue.Activitiy.Adapter.HashMapAdapter;
+import pekl.gasqueue.com.gasqueue.Cart;
+import pekl.gasqueue.com.gasqueue.Product;
 import pekl.gasqueue.com.gasqueue.R;
 
 public class CartActivity extends AppCompatActivity {
-
+    private Cart cart = new Cart();
+    private HashMapAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        adapter = new HashMapAdapter(cart.getCart());
+        listView.setAdapter(adapter);
     }
+
 }
