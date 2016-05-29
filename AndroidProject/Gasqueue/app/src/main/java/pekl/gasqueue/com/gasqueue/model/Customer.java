ef36446b8c23894130be07e1ea5fe796e7fc568d
KevinCT <kevinchentrieu@gmail.com>
2016-05-29 22:21:56 +0200

    changed some gui

--- AndroidProject/Gasqueue/app/src/main/java/pekl/gasqueue/com/gasqueue/model/Customer.java
+++ AndroidProject/Gasqueue/app/src/main/java/pekl/gasqueue/com/gasqueue/model/Customer.java
@@ -20,7 +20,7 @@ public class Customer {
     private boolean orderSent;
     private Integer queuePosition; //change this back to nothing after test
     private boolean banned = false;
-    private String clientID = "asdsad";
+    private String clientID = "fdhdh";
 
     private Cart cart;
     public StopWatch timer = new StopWatch();
